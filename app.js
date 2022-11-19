7525c77f80122f0611ce960aadfa5e36e7755e3d
CedricNiyiVinci <cedric.niyikiza@student.vinci.be>
2022-11-19 18:28:40 +0100

    add - cors dependancy

--- app.js
+++ app.js
@@ -2,6 +2,11 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const cors = require('cors');
+
+const corsOptions = {
+    origin: 'http://localhost:3000',
+  };
 
 var productRouter = require('./routes/products');
 var usersRouter = require('./routes/users');
@@ -13,7 +18,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-app.use('/products', productRouter);
+app.use('/products', cors(corsOptions),  productRouter);
 app.use('/users', usersRouter);
 
 module.exports = app;
